b2524f17c6a53ce25a147a49c86a19e7838ed74f
osinot <osinot@gmail.com>
2021-01-24 23:47:31 +0300

    burc rehberi deneme uygulaması merhaba flutter

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'eski.dart';
+
+void main() {
+  runApp(MyApp());  
+}
+
+class MyApp extends StatelessWidget {
+  
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        secondaryHeaderColor: Colors.teal,
+        backgroundColor: Colors.yellowAccent,
+        scaffoldBackgroundColor: Colors.green,
+        primarySwatch: Colors.purple,
+             
+        
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
