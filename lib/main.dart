f56f8f238db3f735f1752e76e9ee958b406ac837
osinot <osinot@gmail.com>
2021-01-25 10:14:39 +0300

    testzgsds

--- lib/main.dart
+++ lib/main.dart
@@ -2,23 +2,20 @@ import 'package:flutter/material.dart';
 import 'eski.dart';
 
 void main() {
-  runApp(MyApp());  
+  runApp(MyApp());
 }
 
+// asda
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        
         secondaryHeaderColor: Colors.teal,
         backgroundColor: Colors.yellowAccent,
         scaffoldBackgroundColor: Colors.green,
         primarySwatch: Colors.purple,
-             
-        
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
